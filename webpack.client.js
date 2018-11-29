a50658585cf67d70a6e2e34d0a1858b49164d843
RSN <ridoansalehnasution98@gmail.com>
2018-11-29 19:51:49 +0700

    add file-loader (#1)

--- webpack.client.js
+++ webpack.client.js
@@ -1,6 +1,7 @@
-const { publicPath, assetsPath, commonLoaders } = require('./common.config');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const path = require('path');
+import WriteFilePlugin from 'write-file-webpack-plugin';
+const { publicPath, assetsPath, commonLoaders } = require('./common.config');
 
 module.exports = {
   mode: 'development',
@@ -22,6 +23,21 @@ module.exports = {
           'css-loader?modules&localIdentName=[name]__[local]___[hash:base64:5]',
         ],
       },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: assetsPath,
+              publicPath
+            }
+          }
+        ]
+      }
     ]),
   },
+  plugins: [
+    new WriteFilePlugin()
+  ]
 };
