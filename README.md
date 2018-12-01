1c3c6ea6952ad02c6bff6f875ae47b18238b7b53
RSN <ridoansalehnasution98@gmail.com>
2018-12-01 14:10:32 +0700

    add react-router-dom (#2)

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Codeburst SSR
+
+## Folder Structure
+
+Here is our folder structure for the React SSR project template 
+
+-codeburst-ssr
+ |_src/
+ |   |_components
+ |   |_containers
+ |   |_css
+ |   |_reducers
+ |   |_routes.js
+ |   |_server.js
+ |   |_client.js
+ |   |_index.js
+ |_.babelrc
+ |_.gitignore
+ |_package.json
+ |_config.js
+ |_README.md
+ |_webpack.common.js
+ |_webpack.server.js
+ |_webpack.client.js
+ |_yarn.lock
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../css/App.css';
+import minion from '../static/minion.gif';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div className={styles.wrapper}>
+            <p>Hello world!</p>
+            <img src={minion} alt='Icon' height='100' width='100' />
+        </div>
+        <p>
+            <Link to='about'>Lihat About</Link>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home
