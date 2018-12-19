295ac5ac1377550f71754cb97848ded7bb4e4970
ridoansaleh <ridoansalehnasution98@gmail.com>
2018-12-19 08:08:06 +0700

    add production config

---
+++ config/dev/webpack.server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const merge = require('webpack-merge');
+const commonConfig = require('../webpack.common');
+
+const serverConfig = {
+  mode: 'development',
+  name: 'server',
+  target: 'node',
+  externals: nodeExternals(),
+  context: path.join(__dirname, '..', '..', 'src'),
+  entry: './appServer.js',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: 'css-loader/locals?modules&localIdentName=[name]__[local]___[hash:base64:5]'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              emitFile: false
+            }
+          }
+        ]
+      } 
+    ],
+  },
+  output: {
+    path: path.join(__dirname, '..', '..', 'dist'),
+    filename: 'server.js',
+    libraryTarget: 'commonjs2',
+    publicPath: '/',
+  }
+};
+
+module.exports = merge(commonConfig, serverConfig);
